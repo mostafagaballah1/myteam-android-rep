bbe72b26bf3fd67c6b844a265f899dae4e987216
Mostafa <mostafa.gaballah@microdoers.net>
2016-07-27 14:40:03 +0300

    1

--- app/src/main/java/com/mostafa/myteam/LoginActivity.java
+++ app/src/main/java/com/mostafa/myteam/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
+
     @Bind(R.id.input_email) EditText _emailText;
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.btn_login) Button _loginButton;
